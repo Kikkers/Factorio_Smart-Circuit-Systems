1d1ede8415bd70cb395e9fd7af4ca92c09fdc169
Kikkers <mattijsdriel@gmail.com>
2014-11-03 21:10:28 +0100

    File cleanup

--- Smart-Circuit-Systems_0.2.0/data.lua
+++
@@ -1,7 +0,0 @@
-require("prototypes.item")
-require("prototypes.common-entity")
-require("prototypes.tank-entity")
-require("prototypes.pump-entity")
-require("prototypes.pole-entity")
-require("prototypes.accu-entity")
-require("prototypes.recipe")
