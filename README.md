75900555eaa66bc5a6565accd287fc31e1547093
Kikkers <mattijsdriel@gmail.com>
2014-11-15 00:29:13 +0100

    Updated for 0.2.2, robots mostly work correctly.

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 Factorio: Smart-Circuit-Systems
 =========================
 
-Tested to work with Factorio v0.11.1
+Tested to work with Factorio v0.11.3
 
-Mod for factorio that (currently) adds four items, both usable as elements of the circuit network: 
+Mod for factorio that (currently) adds four items, all capable of interacting with the circuit network: 
 + A sensor for the contents of a pipe.
 + A pump that works on a wire condition.
 + A wire switcher that works on a wire condition
 + A sensor for the charge of an accumulator
 
-Mod was formerly known as Smart-Fluid, the two are NOT compatible
+(Mod was formerly known as Smart-Fluid.)
 
-For the impatient though, because official factorio development will likely eventually include exactly the contents of this mod (although significantly less hack-y)
+This mod is for the impatient, because official factorio development will likely eventually include exactly the contents of this mod in the next major version update (although significantly less hack-y)
 
 Install instructions:
 Copy the "Smart-Circuit_Systems_..." folder into the mods folder. The mods folder is created after running Factorio once. After copying, restart Factorio and open mods in the main menu, you should see the mod already enabled there.
@@ -20,12 +20,15 @@ Stuff to know / how to use / what hacks were used:
 + Smart tanks and smart accumulators create dummy items representing their measurements. All dummy items stack up to 1000, which is the maximum of the sensor's respective measurements.
 + The pump and electric switch have multiple selection boxes. Connect red or green wires to the selection box that shows the yellow junk, in order to allow setting the circuit conditions. 
 + The electric switch has two additional terminals on which you can connect wires (copper, red or green), through which you can control what signals or power is passed on.
-+ Concerning the yellow junk inside the pump selection box, ignore it. That's just the hidden inserter putting items from the hidden chest into the hidden chest (if you must ask, check the code). 
++ Concerning the yellow junk inside the pump selection box, ignore it. That's just the hidden inserter checking the circuit condition. 
 
 Known issues:
-+ The new structures can't yet be constructed using construction bots (rather the structures will be built but non-functional). I'm waiting for a scripting update to fix this.
++ The way that the smart pump is currently set up prevents it from appearing in blueprints or as a ghost. I'm guessing the best solution would be a multi-tile structure like the electric switch.
 
 Version history:
+
+0.2.2
++ All structures except for the smart pump can now be constructed and deconstructed using robots. 
 
 0.2.1
 + Added technologies for the objects. Required tech is fluid handling, advanced electronics, electric energy distribution
---
+++ Smart-Circuit-Systems_0.2.2/control.lua
@@ -0,0 +1,108 @@
+require "defines"
+require "scripts.common"
+require "scripts.pole"
+require "scripts.tank"
+require "scripts.pump"
+require "scripts.accu"
+
+game.oninit(InitCommon)
+game.onload(InitCommon)
+
+game.onevent(defines.events.onplayerrotatedentity, function(event)
+	if event.entity.name == "smart-pump-actuator" then
+		local otherPart = GetEntityAt(event.entity.position, "smart-pump-pipe")
+		otherPart.direction = event.entity.direction
+	elseif event.entity.name == "smart-pump-pipe" then
+		local otherPart = GetEntityAt(event.entity.position, "smart-pump-actuator")
+		otherPart.direction = event.entity.direction
+	elseif event.entity.name == "pole-actuator" then
+		event.entity.direction = (event.entity.direction + 6) % 8
+	end
+end)
+
+game.onevent(defines.events.ontick, function(event)
+
+	-- go over all smart poles
+	for _,ys in pairs(glob.allPoles) do
+		for _,pole in pairs(ys) do
+			if pole.valid then 
+				TickPole(pole)
+			end
+		end
+	end
+
+	-- go over all smart batteries
+	for _,ys in pairs(glob.allBatteries) do
+		for _,battery in pairs(ys) do
+			if battery.valid then 
+				TickBattery(battery)
+			end
+		end
+	end
+
+	-- go over all smart tanks
+	for _,ys in pairs(glob.allTanks) do
+		for _,tank in pairs(ys) do
+			if tank.valid then 
+				TickTank(tank)
+			end
+		end
+	end
+	
+	-- go over all smart pumps
+	for _,ys in pairs(glob.allPumps) do
+		for _,pump in pairs(ys) do
+			if pump.valid then 
+				TickPump(pump) 
+			end
+		end
+	end
+end)
+
+onbuiltentityHandler = {
+	["smart-battery"] = InitBattery,
+	["pole-actuator"] = InitPole,
+	["smart-pump-pipe"] = InitPump,
+	["smart-tank"] = InitTank,
+}
+game.onevent(defines.events.onbuiltentity, function(event)
+	local func = onbuiltentityHandler[event.createdentity.name]
+	if func ~= nil then func(event.createdentity) end
+end)
+
+game.onevent(defines.events.onrobotbuiltentity, function(event)
+	local func = onbuiltentityHandler[event.createdentity.name]
+	if func ~= nil then func(event.createdentity) end
+end)
+
+onpreplayermineditemHandler = {
+	["smart-tank" 		   ] = CleanupTank,
+	["smart-battery"       ] = CleanupBattery,
+	["smart-battery-chest" ] = CleanupBatteryChest,
+	["smart-pump-pipe"     ] = CleanupPump,
+	["smart-pump-actuator" ] = CleanupPumpActuator,
+	["pole-actuator"       ] = CleanupPole,
+	["west-node"     ] = CleanupPoleNode,
+	["east-node"     ] = CleanupPoleNode,
+	["north-node"     ] = CleanupPoleNode,
+	["south-node"     ] = CleanupPoleNode,
+}
+game.onevent(defines.events.onpreplayermineditem, function(event)
+	local func = onpreplayermineditemHandler[event.entity.name]
+	if func ~= nil then func(event.entity) end
+end)
+game.onevent(defines.events.onrobotpremined, function(event)
+	local func = onpreplayermineditemHandler[event.entity.name]
+	if func ~= nil then func(event.entity) end
+end)
+
+onentitydiedHandler = {
+	["smart-tank" 		   ] = CleanupTank,
+	["smart-pump-pipe"     ] = CleanupPump,
+	["smart-battery"       ] = CleanupBattery,
+	["pole-actuator"       ] = CleanupPole,
+}
+game.onevent(defines.events.onentitydied, function(event)
+	local func = onentitydiedHandler[event.entity.name]
+	if func ~= nil then func(event.entity) end
+end)
